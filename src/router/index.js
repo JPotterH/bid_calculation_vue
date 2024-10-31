13fb4053f1ba7b2e6a36ab398dd8e64f2a42de2f
Juan M. Lauredo <jpotterh@gmail.com>
2024-10-31 17:04:22 -0400

    updated router to load layouts

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { loadLayoutMiddleware } from "@/router/middleware/loadLayoutMiddleware";
 import HomeView from "@/views/HomeView.vue";
 import BidCalculatorView from "@/views/BidCalculatorView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
@@ -9,16 +10,25 @@ const routes = [
     path: route.home,
     name: "Home",
     component: HomeView,
+    // meta: {
+    //   layout: "AppDefaultLayout",
+    // },
   },
   {
     path: route.bidcalculator,
     name: "BidCalculator",
     component: BidCalculatorView,
+    // meta: {
+    //   layout: "AppDefaultLayout",
+    // },
   },
   {
     path: "/:pathMatch(.*)*",
     name: "NotFound",
     component: NotFoundView,
+    meta: {
+      layout: "AppErrorLayout",
+    },
   },
 ];
 
@@ -26,5 +36,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach(loadLayoutMiddleware);
 
 export default router;
